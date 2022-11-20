7d7c23def350846b1bba903bc26383cc2eca1b81
Richard Kell <rickakell1@gmail.com>
2022-11-20 14:32:16 -0600

    Clean up the code: move to functions, add main()

--- polar_clock.py
+++ polar_clock.py
@@ -1,39 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.axes(projection = 'polar')
-
-# How close should we plot points?
-POINT_RESOLUTION = 0.01
-
-def plot_circle(radius):
+def plot_circle(radius, resolution):
     # Array of theta values from 0 to 2pi
-    thetas = np.arange(0, (2 * np.pi), POINT_RESOLUTION)
+    thetas = np.arange(0, (2 * np.pi), resolution)
 
     # Plotting the circle
     for theta in thetas:
         plt.polar(theta, radius, 'g.')
 
-def plot_line(angle, length):
+def plot_line(angle, length, resolution):
     # Array of radii from 0 to length of the line
-    points = np.arange(0, length, POINT_RESOLUTION)
+    points = np.arange(0, length, resolution)
 
     # Plotting the line
     for point in points:
         plt.polar(angle, point, 'g.')
 
-
-
-for i in range(0, 10, 5):
-
+def draw_simple_clock(radius):
     # Plot the clock
-    CLOCK_RADIUS = 5
-    plot_circle(CLOCK_RADIUS)
+    plot_circle(radius)
     # Plot the minute hand
-    plot_line(i, CLOCK_RADIUS * .75)
+    plot_line(5, radius * .75)
     # Plot the hour hand
-    plot_line(2, CLOCK_RADIUS * .5)
+    plot_line(2, radius * .5)
 
     # Display the clock plot
     plt.axis('off')
     plt.show()
+
+def main():
+    # How close should we plot points?
+    RESOLUTION = 0.01
+    # Half the name of this project
+    plt.axes(projection = 'polar')
+
+    draw_simple_clock(5, RESOLUTION)
+
+
+if __name__ == "__main__":
+    main()
